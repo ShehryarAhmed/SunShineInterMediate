a198151a134ea07b359712ff28d636a4a67b719d
ShehryaAhmed <shehreyarahmed5@gmail.com>
2016-12-27 02:31:27 +0500

    change on res file add menu file

--- SunShine/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ SunShine/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -9,9 +9,13 @@ import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
 
 import android.os.Bundle;
 import android.support.v7.view.menu.MenuAdapter;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.net.URL;
+import java.sql.Time;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mWeatherTextView;
@@ -22,11 +26,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
 
+        loadWeatherData();
+
     }
 
     private void loadWeatherData() {
         String location = SunshinePreferences.getPreferredWeatherLocation(this);
+new FetchWeatherTask().execute(location);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.forcast_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int get = item.getItemId();
+        switch (get){
+            case R.id.refresh :
+                //refresh
+                Toast.makeText(MainActivity.this, "Refresh", Toast.LENGTH_SHORT).show();
+break;
+            
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
@@ -58,4 +83,5 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
 }
