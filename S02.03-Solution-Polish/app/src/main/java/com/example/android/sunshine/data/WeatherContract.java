210adf244e55affeb9cb0a3199c9a47ebcf6683c
shehryarAhmed <shehreyarahmed5@gmail.com>
2017-01-22 21:45:01 +0500

    weather contract class

---
+++ S02.03-Solution-Polish/app/src/main/java/com/example/android/sunshine/data/WeatherContract.java
@@ -0,0 +1,15 @@
+package com.example.android.sunshine.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by android on 1/22/2017.
+ */
+
+public class WeatherContract {
+    public static final class WeatherEntry implements BaseColumns{
+        public static final String TABLE_NAME = "weather";
+
+        
+    }
+}
