21a0828c82b8164ff602651c4a9a4669ac8064c6
shehryarAhmed <shehreyarahmed5@gmail.com>
2017-02-06 09:43:05 +0500

    working start on contentprovider

--- S02.03-Solution-Polish/app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
+++ S02.03-Solution-Polish/app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
@@ -12,7 +12,7 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "weather.db";
 
-    public static final int DATABASE_VERSION = 2;
+    public static final int DATABASE_VERSION = 3;
 
     public WeatherDbHelper(Context context){
         super(context,DATABASE_NAME,null,DATABASE_VERSION);
@@ -20,15 +20,19 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        final String SQL_CREATE_WEATHER_TABLE = "CREATE TABLE " + WeatherContract.WeatherEntry.TABLE_NAME +
-                " ("+ WeatherContract.WeatherEntry._ID + " INTEGER PREIMARY KEY AUTOINCREMENT, " +
+        final String SQL_CREATE_WEATHER_TABLE =
+                "CREATE TABLE " + WeatherContract.WeatherEntry.TABLE_NAME + " ("+
+                WeatherContract.WeatherEntry._ID + " INTEGER PREIMARY KEY AUTOINCREMENT," +
                 WeatherContract.WeatherEntry.CoLUMN_DATE + " INTEGER NOT NULL, " +
+                WeatherContract.WeatherEntry.COLUMN_WEATHER_ID + "INTEGER NOT NULL," +
                 WeatherContract.WeatherEntry.COLUMN_MIN_TEMP + " REAL NOT NULL, "+
                 WeatherContract.WeatherEntry.COLUMN_MAX_TEMP + " REAL NOT NULL, " +
                 WeatherContract.WeatherEntry.COLUMN_HUMIDTIY + " REAL NOT NULL, " +
                 WeatherContract.WeatherEntry.COLUMN_PRESSURE + " REAL NOT NULL, " +
                 WeatherContract.WeatherEntry.COLUMN_WING_SPEED + " REAL NOT NULL, " +
-                WeatherContract.WeatherEntry.COLUMN_DEGREES + " REAL NOT NULL" + ");";
+                WeatherContract.WeatherEntry.COLUMN_DEGREES + " REAL NOT NULL, " +
+                        " UNIQUE (" + WeatherContract.WeatherEntry.CoLUMN_DATE + ") ON CONFLICT REPLACE);";
+
         sqLiteDatabase.execSQL(SQL_CREATE_WEATHER_TABLE);
 
 
