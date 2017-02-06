aca07182c800db5ed8cbbb09b1c964e1abcda29d
shehryarAhmed <shehreyarahmed5@gmail.com>
2017-02-06 10:23:58 +0500

    ALHUMDULILLAH work on on shareprefChanges

--- S02.03-Solution-Polish/app/src/main/java/com/example/android/sunshine/SettingsFragment.java
+++ S02.03-Solution-Polish/app/src/main/java/com/example/android/sunshine/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.example.android.sunshine;
 
+import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.preference.CheckBoxPreference;
@@ -7,6 +8,9 @@ import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceScreen;
+
+import com.example.android.sunshine.data.SunshinePreferences;
+import com.example.android.sunshine.data.WeatherContract;
 
 import static com.example.android.sunshine.R.style.Preference;
 
@@ -16,12 +20,20 @@ import static com.example.android.sunshine.R.style.Preference;
 
 public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
     @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
 
-        android.support.v7.preference.Preference preference = findPreference(s);
+        Activity activity = getActivity();
+        if(key.equals(getString(android.R.string.pref_location_key))){
+            SunshinePreferences.resetLocationCoordinates(activity);
+        }
+        else if(key.equals(getString(android.R.string.pref_units_key))) {
+            activity.getContentResolver().notifyChange(WeatherContract.WeatherEntry.CONTENT_URI,null);
+        }
+
+        android.support.v7.preference.Preference preference = findPreference(key);
         if(null != preference){
             if(!(preference instanceof CheckBoxPreference)){
-                setPreferenceSummary(preference,sharedPreferences.getString(s,""));
+                setPreferenceSummary(preference,sharedPreferences.getString(key,""));
             }
         }
     }
--- S02.03-Solution-Polish/app/src/main/java/com/example/android/sunshine/data/WeatherContract.java
+++ S02.03-Solution-Polish/app/src/main/java/com/example/android/sunshine/data/WeatherContract.java
@@ -7,6 +7,8 @@ import android.provider.BaseColumns;
  */
 
 public class WeatherContract {
+
+
     public static final class WeatherEntry implements BaseColumns{
         public static final String TABLE_NAME = "weather";
 
