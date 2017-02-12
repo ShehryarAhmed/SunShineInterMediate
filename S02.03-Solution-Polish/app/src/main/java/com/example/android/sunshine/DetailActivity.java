de45bd472169355d96dfae2463553c0915a3af4f
shehryarAhmed <shehreyarahmed5@gmail.com>
2017-02-12 10:31:33 +0500

    working on OnCreateMethod in detail Activity

--- S02.03-Solution-Polish/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S02.03-Solution-Polish/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,41 +1,80 @@
 package com.example.android.sunshine;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
 import android.support.v4.app.ShareCompat;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.example.android.sunshine.data.WeatherContract;
+
 /**
  * Created by android on 1/15/2017.
  */
 
-public class DetailActivity extends AppCompatActivity{
+public class DetailActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
     public static final String FORCAST_SHARE_HASHTAG = "#SUNSHINE";
+
+
+
+
+    public static final String[] WEATHER_DETAIL_PROJECTION = {
+                        WeatherContract.WeatherEntry.COLUMN_DATE,
+                        WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+                        WeatherContract.WeatherEntry.COLUMN_MIN_TEMP,
+                        WeatherContract.WeatherEntry.COLUMN_HUMIDITY,
+                        WeatherContract.WeatherEntry.COLUMN_PRESSURE,
+                        WeatherContract.WeatherEntry.COLUMN_WIND_SPEED,
+                        WeatherContract.WeatherEntry.COLUMN_DEGREES,
+                        WeatherContract.WeatherEntry.COLUMN_WEATHER_ID
+               };
+
+        public static final int INDEX_WEATHER_DATE = 0;
+        public static final int INDEX_WEATHER_MAX_TEMP = 1;
+        public static final int INDEX_WEATHER_MIN_TEMP = 2;
+        public static final int INDEX_WEATHER_HUMIDITY = 3;
+        public static final int INDEX_WEATHER_PRESSURE = 4;
+        public static final int INDEX_WEATHER_WIND_SPEED = 5;
+        public static final int INDEX_WEATHER_DEGREES = 6;
+        public static final int INDEX_WEATHER_CONDITION_ID = 7;
+
+    private static final int ID_DETAIL_LOADER = 353;
 
     private String mForecast;
 
-    private TextView mWeatherDisplay;
+    private Uri mUri;
+
+        private TextView mDateView;
+        private TextView mDescriptionView;
+        private TextView mHighTemperatureView;
+       private TextView mLowTemperatureView;
+        private TextView mHumidityView;
+        private TextView mWindView;
+        private TextView mPressureView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        mWeatherDisplay = (TextView) findViewById(R.id.tv_display_weather);
+        mDateView = (TextView) findViewById(R.id.date);
+                mDescriptionView = (TextView) findViewById(R.id.weather_description);
+                mHighTemperatureView = (TextView) findViewById(R.id.high_temperature);
+                mLowTemperatureView = (TextView) findViewById(R.id.low_temperature);
+                mHumidityView = (TextView) findViewById(R.id.humidity);
+                mWindView = (TextView) findViewById(R.id.wind);
+                mPressureView = (TextView) findViewById(R.id.pressure);
+        mUri = getIntent().getData();
 
-        Intent intentTheStartedThisActivity = getIntent();
-
-        if(intentTheStartedThisActivity != null){
-        if(intentTheStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)){
-            mForecast = intentTheStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
-            mWeatherDisplay.setText(mForecast);
-
-        }
-    }}
+        
+}
 
 
 
@@ -72,6 +111,21 @@ public class DetailActivity extends AppCompatActivity{
                 .getIntent();
         shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
         return shareIntent;
+
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
 
     }
 }
